5a1c1d2ce6cf05a34f803962b8662a99bdd6862d
edmond <edmond.wanyama@student.moringaschool.com>
2023-07-20 16:44:12 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    const handleButtonFocus = (e) => {
+      e.preventDefault();
+      console.log("Good!");
+    };
+    const handleButtonBlur = (e) => {
+      e.preventDefault();
+      console.log("Hey! Eyes on me!");
+    };
+    return (
+      <div>
+        <button onFocus={handleButtonFocus} onBlur={handleButtonBlur}>
+          Eyes on me
+        </button>
+      </div>
+    );
+  }
+  
+  export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const handleChange = (event) => {
+    console.log("Entering password...");
+  };
+  return (
+    <div>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
